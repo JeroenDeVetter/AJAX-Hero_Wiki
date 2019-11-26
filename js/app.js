1bb3d983a36101f9c14a9f051390cffea183c193
Jeroen De Vetter <devetterjeroen@gmail.com>
2019-11-26 11:02:45 +0100

    refactored ?

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ function createTag(content, idName, tagName) {
   element.setAttribute("id", idName);
   element.innerHTML = content;
   return element;
-}
+};
 
 function createImage(src) {
   const img = document.createElement("img");
   img.setAttribute("src", src);
   return img;
-}
+};
 
 function toggle() {
           switch (this.innerHTML) {
@@ -88,13 +88,169 @@ function toggle() {
                 }
               break;
           }
-        };
+};
 
 function createWrapper(idName, tagName) {
   const element = document.createElement(tagName);
   element.setAttribute("id", idName);
   return element;
-}
+};
+
+function createAppererance(result) {
+
+  const appearance = createWrapper("appearance", "div");
+  appearance.appendChild(createTag("Eyecolor", "collapsible", "h2"));
+  appearance.appendChild(
+    createTag(result.eyeColor, "eyecolor", "div")
+  );
+  appearance.appendChild(createTag("Gender", "collapsible", "h2"));
+  appearance.appendChild(
+    createTag(result.gender, "gender", "div")
+  );
+  appearance.appendChild(createTag("HairColor", "collapsible", "h2"));
+  appearance.appendChild(
+    createTag(result.hairColor, "haircolor", "div")
+  );
+  appearance.appendChild(createTag("Race", "collapsible", "h2"));
+  appearance.appendChild(createTag(result.race, "race", "div"));
+  appearance.appendChild(createTag("Height", "collapsible", "h2"));
+  appearance.appendChild(
+    createTag(
+      "Height: " +
+        result.height[0] +
+        "ft " +
+        result.height[1],
+      "height",
+      "div"
+    )
+  );
+  appearance.appendChild(createTag("Weight", "collapsible", "h2"));
+      appearance.appendChild(
+        createTag(
+          "Weight: " +
+            result.weight[0] +
+            "lbs " +
+            result.weight[1],
+          "weight",
+          "div"
+        )
+      );
+  return appearance;
+};
+
+function createBiograpphy(result) {
+  const biography = createWrapper("biography", "div");
+
+  biography.appendChild(createTag("Aliases", "collapsible", "h2"));
+  result.aliases.forEach(element => {
+    const spans = document.createElement("span");
+    const aliases = createWrapper("Aliases", "div");
+    spans.innerHTML = element;
+    aliases.appendChild(spans);
+    biography.appendChild(aliases);
+  });
+  biography.appendChild(createTag("Aligment", "collapsible", "h2"));
+  biography.appendChild(
+    createTag("Aligment: " + result.alignment, "aligment", "div")
+  );
+  biography.appendChild(createTag("AlterEgo", "collapsible", "h2"));
+  biography.appendChild(
+    createTag("AlterEgo: " + result.alterEgos, "alterEgos", "div")
+  );
+  biography.appendChild(createTag("First Appearance", "collapsible", "h2"));
+  biography.appendChild(
+    createTag(
+      "FirstAppearance: " + result.firstAppearance,
+      "firstappearance",
+      "div"
+    )
+  );
+  biography.appendChild(createTag("FullName", "collapsible", "h2"));
+  biography.appendChild(
+    createTag("FullName" + result.fullName, "fullname", "div")
+  );
+  biography.appendChild(createTag("PlaceofBirth", "collapsible", "h2"));
+  biography.appendChild(
+    createTag(
+      "PlaceOfBirth: " + result.placeOfBirth,
+      "placeofbirth",
+      "div"
+    )
+  );
+  biography.appendChild(createTag("Publisher", "collapsible", "h2"));
+  biography.appendChild(
+    createTag(
+      "Publisher: " + result.publisher,
+      "publisher",
+      "div"
+    )
+  );
+  return biography;
+};
+
+function createContact(result) {
+  const connections = createWrapper("connections", "div");
+
+  connections.appendChild(
+    createTag("Group Affiliation", "collapsible", "h2")
+  );
+  connections.appendChild(
+    createTag(result.groupAffiliation, "affiliation", "div")
+  );
+  const splitted = result.relatives.split(",");
+  connections.appendChild(createTag("Relatives", "collapsible", "h2"));
+  splitted.forEach(element => {
+    connections.appendChild(createTag(element, "Relatives", "div"));
+  });
+
+  return connections;
+};
+
+function createPowerStat(result) {
+  const powerstats = createWrapper("powerstats", "div");
+
+  
+   powerstats.appendChild(createTag("Combat", "collapsible", "h2"));
+   powerstats.appendChild(
+     createTag(result.combat, "combat", "div")
+   );
+   powerstats.appendChild(createTag("Durablitity", "collapsible", "h2"));
+   powerstats.appendChild(
+     createTag(result.durability, "durablitity", "div")
+   );
+   powerstats.appendChild(createTag("Intelligence", "collapsible", "h2"));
+   powerstats.appendChild(
+     createTag(result.intelligence, "intelligence", "div")
+   );
+   powerstats.appendChild(createTag("Power", "collapsible", "h2"));
+   powerstats.appendChild(
+     createTag(result.power, "power", "div")
+   );
+   powerstats.appendChild(createTag("Speed", "collapsible", "h2"));
+   powerstats.appendChild(
+     createTag(result.speed, "speed", "div")
+   );
+   powerstats.appendChild(createTag("Strenght", "collapsible", "h2"));
+   powerstats.appendChild(
+     createTag(result.strength, "strenght", "div")
+   );
+   return powerstats;
+};
+
+function createWork(result) {
+        const work = createWrapper("work", "div");
+
+  work.appendChild(createTag("Base", "collapsible", "h2"));
+      work.appendChild(createTag(result.base, "base", "div"));
+      work.appendChild(createTag("Occupation", "collapsible", "h2"));
+      work.appendChild(createTag(result.occupation, "occupation", "div"));
+      return work;
+};
+
+function resetTarget(result) {
+  result.innerHTML = "";
+  return result;
+};
 
 async function getHero() {
   const input = document.getElementById("input").value;
@@ -107,165 +263,41 @@ async function getHero() {
   })
     .then(res => res.json())
     .then(result => {
-      const mainWrapper = createWrapper("main-wrapper", "div");
+      resetTarget(document.getElementById('target'));
       const wrapper = createWrapper("wrapper", "div");
       const wrapper1 = createWrapper("wrapper1", "div");
-      const appearance = createWrapper("appearance", "div");
-      const biography = createWrapper("biography", "div");
-      const connections = createWrapper("connections", "div");
-      const powerstats = createWrapper("powerstats", "div");
-      const work = createWrapper("work", "div");
       const images = createWrapper("image", "div");
       wrapper.appendChild(createTag(result.name, "heroName", "h1"));
 
-      //appearance Generate
-      appearance.appendChild(createTag("Eyecolor", "collapsible", "h2"));
-      appearance.appendChild(
-        createTag(result.appearance.eyeColor, "eyecolor", "div")
-      );
-      appearance.appendChild(createTag("Gender", "collapsible", "h2"));
-      appearance.appendChild(
-        createTag(result.appearance.gender, "gender", "div")
-      );
-      appearance.appendChild(createTag("HairColor", "collapsible", "h2"));
-      appearance.appendChild(
-        createTag(result.appearance.hairColor, "haircolor", "div")
-      );
-      appearance.appendChild(createTag("Race", "collapsible", "h2"));
-      appearance.appendChild(createTag(result.appearance.race, "race", "div"));
-      appearance.appendChild(createTag("Height", "collapsible", "h2"));
-      appearance.appendChild(
-        createTag(
-          "Height: " +
-            result.appearance.height[0] +
-            "ft " +
-            result.appearance.height[1],
-          "height",
-          "div"
-        )
-      );
-      appearance.appendChild(createTag("Weight", "collapsible", "h2"));
-      appearance.appendChild(
-        createTag(
-          "Weight: " +
-            result.appearance.weight[0] +
-            "lbs " +
-            result.appearance.weight[1],
-          "weight",
-          "div"
-        )
-      );
-      wrapper1.appendChild(appearance);
-
-      // imgage generate
-
+      //appearance Generat  
+      wrapper1.appendChild(createAppererance(result.appearance));
+      // imgage generate no refractering needed way to little code
       images.appendChild(createImage(result.images.lg));
       wrapper.appendChild(images);
       //biography Generate
-      biography.appendChild(createTag("Aliases", "collapsible", "h2"));
-      result.biography.aliases.forEach(element => {
-        const spans = document.createElement("span");
-        const aliases = createWrapper("Aliases", "div");
-        spans.innerHTML = element;
-        aliases.appendChild(spans);
-        biography.appendChild(aliases);
-      });
-      biography.appendChild(createTag("Aligment", "collapsible", "h2"));
-      biography.appendChild(
-        createTag("Aligment: " + result.biography.alignment, "aligment", "div")
-      );
-      biography.appendChild(createTag("AlterEgo", "collapsible", "h2"));
-      biography.appendChild(
-        createTag("AlterEgo: " + result.biography.alterEgos, "alterEgos", "div")
-      );
-      biography.appendChild(createTag("First Appearance", "collapsible", "h2"));
-      biography.appendChild(
-        createTag(
-          "FirstAppearance: " + result.biography.firstAppearance,
-          "firstappearance",
-          "div"
-        )
-      );
-      biography.appendChild(createTag("FullName", "collapsible", "h2"));
-      biography.appendChild(
-        createTag("FullName" + result.biography.fullName, "fullname", "div")
-      );
-      biography.appendChild(createTag("PlaceofBirth", "collapsible", "h2"));
-      biography.appendChild(
-        createTag(
-          "PlaceOfBirth: " + result.biography.placeOfBirth,
-          "placeofbirth",
-          "div"
-        )
-      );
-      biography.appendChild(createTag("Publisher", "collapsible", "h2"));
-      biography.appendChild(
-        createTag(
-          "Publisher: " + result.biography.publisher,
-          "publisher",
-          "div"
-        )
-      );
-      wrapper1.appendChild(biography);
+     
+      wrapper1.appendChild(createBiograpphy(result.biography));
 
       //connections gerete
-      connections.appendChild(
-        createTag("Group Affiliation", "collapsible", "h2")
-      );
-      connections.appendChild(
-        createTag(result.connections.groupAffiliation, "affiliation", "div")
-      );
-      const splitted = result.connections.relatives.split(",");
-      connections.appendChild(createTag("Relatives", "collapsible", "h2"));
-      splitted.forEach(element => {
-        connections.appendChild(createTag(element, "Relatives", "div"));
-      });
-      wrapper1.appendChild(connections);
+      wrapper1.appendChild(createContact(result.connections));
 
-      //Powerstats ganerate
-      powerstats.appendChild(createTag("Combat", "collapsible", "h2"));
-      powerstats.appendChild(
-        createTag(result.powerstats.combat, "combat", "div")
-      );
-      powerstats.appendChild(createTag("Durablitity", "collapsible", "h2"));
-      powerstats.appendChild(
-        createTag(result.powerstats.durability, "durablitity", "div")
-      );
-      powerstats.appendChild(createTag("Intelligence", "collapsible", "h2"));
-      powerstats.appendChild(
-        createTag(result.powerstats.intelligence, "intelligence", "div")
-      );
-      powerstats.appendChild(createTag("Power", "collapsible", "h2"));
-      powerstats.appendChild(
-        createTag(result.powerstats.power, "power", "div")
-      );
-      powerstats.appendChild(createTag("Speed", "collapsible", "h2"));
-      powerstats.appendChild(
-        createTag(result.powerstats.speed, "speed", "div")
-      );
-      powerstats.appendChild(createTag("Strenght", "collapsible", "h2"));
-      powerstats.appendChild(
-        createTag(result.powerstats.strength, "strenght", "div")
-      );
-      wrapper1.appendChild(powerstats);
+
+     //Powerstats ganerate
+      wrapper1.appendChild(createPowerStat(result.powerstats));
 
       //Work ganerate
-      work.appendChild(createTag("Base", "collapsible", "h2"));
-      work.appendChild(createTag(result.work.base, "base", "div"));
-      work.appendChild(createTag("Occupation", "collapsible", "h2"));
-      work.appendChild(createTag(result.work.occupation, "occupation", "div"));
-      wrapper1.appendChild(work);
+      
+      wrapper1.appendChild(createWork(result.work));
 
-      mainWrapper.appendChild(wrapper1);
       document.getElementById("target").appendChild(wrapper);
-      document.getElementById("target").appendChild(mainWrapper);
+      document.getElementById("target").appendChild(wrapper1);
       const toggel = document.querySelectorAll("#collapsible");
 
       toggel.forEach(element => {
         element.addEventListener("click",toggle)
       });
     });
-}
+};
 
 document.getElementById("test").addEventListener("submit", function(e) {
   getHero();
